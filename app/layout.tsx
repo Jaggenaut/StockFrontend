6ba286ec48302771232bbf3bd334a7832261e300
Aditya <adityasi@iitbhilai.ac.in>
2025-03-18 15:14:02 +0530

    Feat: font dependency error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,6 @@
 import Navbar from "@/components/navbar";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-  display: "swap",
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-  display: "swap",
-});
 
 export const metadata: Metadata = {
   title: "Portfolio Dashboard",
