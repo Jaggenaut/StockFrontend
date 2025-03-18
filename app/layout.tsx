c921514e1f328142c9fd461551165bb281da3db6
Aditya <adityasi@iitbhilai.ac.in>
2025-03-18 15:20:51 +0530

    Feat: font dependency error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,19 @@
 import Navbar from "@/components/navbar";
 import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+  display: "swap",
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export const metadata: Metadata = {
   title: "Portfolio Dashboard",
@@ -17,7 +30,9 @@ export default function RootLayout({
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
-      <body className={`$antialiased`}>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
         <Navbar />
         <main>{children}</main>
       </body>
