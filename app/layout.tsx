53387eab0b9310d6d04d8e8dfa679fb4ce283c6a
Aditya <adityasi@iitbhilai.ac.in>
2025-03-18 15:15:10 +0530

    Feat: font dependency error

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,9 +17,7 @@ export default function RootLayout({
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`$antialiased`}>
         <Navbar />
         <main>{children}</main>
       </body>
